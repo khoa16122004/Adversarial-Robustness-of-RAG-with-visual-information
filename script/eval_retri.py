450f55a7d2b83b233d38c16f85a70de7130a7306
khoa16122004 <khoatr.work@gmail.com>
2025-06-07 10:08:00 +0700

    Update eval_retri.py

--- script/eval_retri.py
+++ script/eval_retri.py
@@ -13,14 +13,21 @@ loader = DataLoader(path=annotation_path,
 start_index = 0
 end_index = 200
 
+total_recall = 0
+count = 0
+
 for i in tqdm(range(start_index, end_index)): 
     question, answer, paths, gt_paths = loader.take_data(i)
     
-    # load retri_paths
     with open(f"{model_name}_retrieval_result/{i}.json") as f:
         retrieved_data = json.load(f)
     retrieved_data_retri = retrieved_data['image_paths'][:k]
+    
     correct = sum([1 for p in retrieved_data_retri if p in gt_paths])
     recall_at_k = correct / len(gt_paths) if len(gt_paths) > 0 else 0
-    print(f"Sample ID: {i}, Recall@{k}: {recall_at_k}")
-    break
+
+    total_recall += recall_at_k
+    count += 1
+
+avg_recall_at_k = total_recall / count if count > 0 else 0
+print(f"Average Recall@{k}: {avg_recall_at_k:.4f}")
