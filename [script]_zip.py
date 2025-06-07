b5d31fd936555e3059bf08374e7172e7f5b1be9c
khoa16122004 <khoatr.work@gmail.com>
2025-06-07 13:45:31 +0700

    Create [script]_zip.py

---
+++ [script]_zip.py
@@ -0,0 +1,13 @@
+import zipfile
+import os
+
+def zip_folder(folder_path, output_zip_path):
+    with zipfile.ZipFile(output_zip_path, 'w', zipfile.ZIP_DEFLATED) as zipf:
+        for root, _, files in os.walk(folder_path):
+            for file in files:
+                file_path = os.path.join(root, file)
+                arcname = os.path.relpath(file_path, folder_path)
+                zipf.write(file_path, arcname)
+
+# Ví dụ sử dụng
+zip_folder('split_corpus', 'split_corpus.zip')
