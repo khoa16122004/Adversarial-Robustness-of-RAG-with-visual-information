b7ed9fd699f3ee633819b017d630f1d8e7b33f28
khoa16122004 <khoatr.work@gmail.com>
2025-06-11 15:48:16 +0700

    update

--- algorithm/retriever.py
+++ algorithm/retriever.py
@@ -32,6 +32,6 @@ if __name__ == "__main__":
     img_files[3].save("test_image.jpg")  # Save the image for testing purposes.
     reader = Retriever(model_name="clip")
     
-    score = reader("blue sky", [img_files[3], img_files[0], img_files[1], img_files[2]])
+    score = reader("orange eyes", [img_files[3], img_files[0], img_files[1], img_files[2]])
     img_files[2].save("retriever_test_image.jpg")  # Save the image for testing purposes.
     print(score)
