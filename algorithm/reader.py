7c20166528e0cef1c5511f42dd60e2c91f242fc8
khoa16122004 <khoatr.work@gmail.com>
2025-06-11 14:57:43 +0700

    Update reader.py

--- algorithm/reader.py
+++ algorithm/reader.py
@@ -54,7 +54,7 @@ if __name__ == "__main__":
     img_files[3].save("test_image.jpg")  # Save the image for testing purposes.
     reader = Reader(model_name="llava")
     
-    outputs = reader.image_to_text("what is color of the eyes of bird?", [img_files[3]])
+    outputs = reader.image_to_text(question, [img_files[3]])
     print(outputs)  # Should print the answer to the question based on the image provided.
             
         
