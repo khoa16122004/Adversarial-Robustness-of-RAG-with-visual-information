5c96f34341614dcf11c972a5111d1d2aa1d63410
khoa16122004 <khoatr.work@gmail.com>
2025-06-11 16:57:03 +0700

    Update algorithm.py

--- algorithm/algorithm.py
+++ algorithm/algorithm.py
@@ -156,7 +156,8 @@ class NSGAII:
 
 
     def solve(self):
-        P = torch.rand(self.population_size, self.n_k, 3, self.w, self.h).cuda() * self.std
+        # P = torch.rand(self.population_size, self.n_k, 3, self.w, self.h).cuda() * self.std
+        P = torch.rand(self.population_size, 3, self.w, self.h).cuda() * self.std
         P_retri_score, P_reader_score = self.fitness(P)
         
 
