ccf1fa7c8812c6b20f9601170dd4fbd57a4e380c
khoa16122004 <khoatr.work@gmail.com>
2025-06-11 23:40:26 +0700

    Update test.py

--- algorithm/test.py
+++ algorithm/test.py
@@ -9,16 +9,6 @@ from algorithm import NSGAII
 
 
 
-#argsd
---annotation_path # default
---dataset_dir # default
--- pop_size # default 50
--- F # defalut 0.9
--- n_k # default 1
--- w # default 224
--- h # default 224
--- max_iter # default 100
--- std # not defauiltm, require
 
 
 annotation_path = "../v1_anno.jsonl"
@@ -35,28 +25,26 @@ h = 224
 max_iter = 100
 std = 0.1
 
-for sample_id in range(len(data)):
-    question, answer, paths, gt_paths = loader.take_data(arsg.ample_id)
-    img_files = [Image.open(path).convert('RGB').resize((w, h)) for path in paths]
-    
-    
-    fitness = MultiScore(reader_name="llava", 
-                        retriever_name="clip", 
-                        question=question, 
-                        corpus=img_files, 
-                        )
+sample_id = 183
+question, answer, paths, gt_paths = loader.take_data(sample_id)
+img_files = [Image.open(path).convert('RGB').resize((w, h)) for path in gt_paths]
+fitness = MultiScore(reader_name="llava", 
+                     retriever_name="clip", 
+                     question=question, 
+                     original_img=img_files[3], 
+                     answer="The bird in the image has orange eyes.")
 
-    algorithm = NSGAII(
-        population_size=pop_size,
-        mutation_rate=mutation_rate,
-        F=F,
-        n_k=n_k,
-        w=w,
-        h=h,
-        max_iter=max_iter,
-        fitness=fitness,
-        std=std,
-        sample_id=sample_id
-    )
+algorithm = NSGAII(
+    population_size=pop_size,
+    mutation_rate=mutation_rate,
+    F=F,
+    n_k=n_k,
+    w=w,
+    h=h,
+    max_iter=max_iter,
+    fitness=fitness,
+    std=std,
+    sample_id=sample_id
+)
 
-    algorithm.solve()
+algorithm.solve()
