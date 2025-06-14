9e7c3c39a015fab5c2f3d20177d8302417dc9e71
khoa16122004 <khoatr.work@gmail.com>
2025-06-14 10:35:26 +0700

    update

--- algorithm/recheck.py
+++ algorithm/recheck.py
@@ -14,62 +14,61 @@ import numpy as np
 def main(args):
     loader = DataLoader(path=args.annotation_path,
                         img_dir=args.dataset_dir)
-
+    with open("run.txt", "r") as f:
+        lines = [int(line.strip()) for line in f.readlines()]
 
     # fitness
     fitness = MultiScore(reader_name="llava", 
                          retriever_name="clip"
                          )
 
-
-    question, answer, paths, gt_paths = loader.take_data(args.id)
-    corpus = [Image.open(path).convert('RGB').resize((args.w, args.h)) for path in paths]
-    
-    # top1 documents
-    sim_scores = fitness.retriever(question, corpus)
-    top1_img = corpus[sim_scores.argmax()]
-    
-    # answer form top1 documents
-    top1_answer = fitness.reader.image_to_text(question, [top1_img])
-    
-    # init fitness data
-    fitness.init_data(question, top1_img, top1_answer)
+    for id in lines:
         
-    # data_runed
-    dir = f"logs/clip_llava_0.1/{args.id}"
-    images_dir = os.path.join(dir, "images")
-    scores_path = os.path.join(dir, "scores.pkl")
-    individual_path = os.path.join(dir, "individuals.pkl")
-    # read
-    with open(scores_path, 'rb') as f:
-        history = pkl.load(open(scores_path, "rb"))[-1]
-    
-    with open(individual_path, 'rb') as f:
-        individual = pkl.load(open(individual_path, "rb"))
-    
-    P_retri_score, P_reader_score, P_adv_imgs = fitness(individual)
-    print("score: ", history)
-    # print("rerun score: ", P_retri_score, P_reader_score)
-    
-    # greedy selection
-    # filtered = history[history[:, 0] < 1]
-    # print(filtered)
-    # if len(filtered) > 0:
-    #     min_idx = np.argmin(filtered[:, 1])
-    #     result = filtered[min_idx]
-    #     print("Score greedy: ", result)
-    # else:
-    #     print("Không có dòng nào thỏa mãn điều kiện.")
-    
-    
-    # imgs = [P_adv_imgs[min_idx], fitness.original_img]
-    P_adv_imgs.append(fitness.original_img)
-    outputs = []
-    for i, img in enumerate(P_adv_imgs):
-        output = fitness.reader.image_to_text(question, [img])
-        outputs.append(output)
-    
-    print("Output: ", outputs)    
+
+        question, answer, paths, gt_paths = loader.take_data(args.id)
+        corpus = [Image.open(path).convert('RGB').resize((args.w, args.h)) for path in paths]
+        
+        # top1 documents
+        sim_scores = fitness.retriever(question, corpus)
+        top1_img = corpus[sim_scores.argmax()]
+        
+        # answer form top1 documents
+        top1_answer = fitness.reader.image_to_text(question, [top1_img])
+        
+        # init fitness data
+        fitness.init_data(question, top1_img, top1_answer)
+            
+        # data_runed
+        dir = f"logs/clip_llava_0.1/{args.id}"
+        images_dir = os.path.join(dir, "images")
+        scores_path = os.path.join(dir, "scores.pkl")
+        individual_path = os.path.join(dir, "individuals.pkl")
+        # read
+        with open(scores_path, 'rb') as f:
+            history = pkl.load(open(scores_path, "rb"))[-1]
+        
+        with open(individual_path, 'rb') as f:
+            individual = pkl.load(open(individual_path, "rb"))
+        
+        
+        P_retri_score, P_reader_score, P_adv_imgs = fitness(individual)
+        valid_mask = (P_retri_score < 1) & (P_reader_score < 1)
+        print("Mask: ", valid_mask)
+        print(history)
+        imgs = []
+        for i in range(len(valid_mask)):
+            imgs.append(valid_mask)        
+        # imgs = [P_adv_imgs[min_idx], fitness.original_img]
+        imgs.append(fitness.original_img)
+        outputs = []
+        for i, img in enumerate(imgs):
+            output = fitness.reader.image_to_text(question, [img])
+            outputs.append(output)
+        
+        
+        print("==*10")
+        print("Question: ", question)
+        print("Output: ", outputs)    
 
     
 
