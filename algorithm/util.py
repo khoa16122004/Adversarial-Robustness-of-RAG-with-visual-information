5b68167d07b3a894cbc9c185623571a527c6187b
khoa16122004 <khoatr.work@gmail.com>
2025-06-14 22:10:58 +0700

    Update util.py

--- algorithm/util.py
+++ algorithm/util.py
@@ -111,6 +111,9 @@ class DataLoader:
 def compute_nlg_metrics(pred, refs):
     refs = [r.lower().strip() for r in refs]
     pred = pred.lower().strip()
+    
+    print(refs)
+    print(pred)
 
     smoothing = SmoothingFunction().method1
     bleu = sentence_bleu([r.split() for r in refs], pred.split(), smoothing_function=smoothing)
