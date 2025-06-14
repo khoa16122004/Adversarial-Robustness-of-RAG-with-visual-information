a9c5dfd9351186a93dfbfdbdb3b8e18404e1e5ae
khoa16122004 <khoatr.work@gmail.com>
2025-06-14 21:16:17 +0700

    Update eval_retri.py

--- algorithm/eval_retri.py
+++ algorithm/eval_retri.py
@@ -5,5 +5,5 @@ loader = DataLoader(
 )
 
 for i in range(len(loader)):
-    question, answer, paths, gt_paths = loader.take_data(i)
-    print(question, answer, paths, gt_paths)
+    question, answer, query, gt_basenames, retri_basenames, retri_imgs = loader.take_data(i)
+    print(question, answer, query, gt_basenames, retri_basenames, retri_imgs)
