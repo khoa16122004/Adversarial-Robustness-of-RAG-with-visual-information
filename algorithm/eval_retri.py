0a1e771b30f2656e12ff305743db52c169a9e78c
khoa16122004 <khoatr.work@gmail.com>
2025-06-14 21:17:56 +0700

    Update eval_retri.py

--- algorithm/eval_retri.py
+++ algorithm/eval_retri.py
@@ -4,6 +4,13 @@ loader = DataLoader(
     retri_dir="retri_result"
 )
 
+hit_rate = 0
 for i in range(len(loader)):
     question, answer, query, gt_basenames, retri_basenames, retri_imgs = loader.take_data(i)
-    print(question, answer, query, gt_basenames, retri_basenames, retri_imgs)
+    
+    for retri_name in retri_basenames:
+        if retri_name in gt_basenames:
+            hit_rate += 1
+            break
+        
+print(hit_rate)
