e11bee4938e6d0b4d8f54f84af296d6c6fce93fb
khoa16122004 <khoatr.work@gmail.com>
2025-06-16 10:22:15 +0700

    Update fitness.py

--- algorithm/fitness.py
+++ algorithm/fitness.py
@@ -95,6 +95,7 @@ if __name__ == "__main__":
     print(all_texts)
     adv_img_tensor = original_img_tensor + torch.rand(3, 312, 312).cuda() * 100
     adv_img = to_pil_image(adv_img_tensor)
+    adv_img.save("test_adv.jpg")
     all_scores, all_texts = fitness.reader(question, [[adv_img]])
     print(all_scores)
     print(all_texts)
